cdc40f42b372d49e6eeda782527955fa2a332c10
hskshdiabka63836286 <166393377+hskshdiabka63836286@users.noreply.github.com>
2024-04-08 19:25:58 +0600

    Update ab.js

--- ab.js
+++ ab.js
@@ -1,4 +1,5 @@
 const {Telegraf} = require('telegraf');
+const {getPic} = require('./a');
 
 const bot = new Telegraf(process.env.TK)
 
@@ -6,3 +7,14 @@ bot.command('start',ctx=>{
   ctx.reply('Welcome to this bot');
 })
 
+bot.command('getpic',ctx=>{
+  const link = ctx.text.replace('/getpic ','');
+  const picLink = getPic(link);
+  if(!picLink){
+    ctx.reply('Profile picture not found');
+    return;
+  }
+  ctx.replyWithPhoto(picLink)
+})
+
+bot.launch()
