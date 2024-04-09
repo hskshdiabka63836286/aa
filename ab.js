b983c7b5b87f2752e09b5415c58cf462f754b134
hskshdiabka63836286 <166393377+hskshdiabka63836286@users.noreply.github.com>
2024-04-09 13:57:33 +0600

    Update ab.js

--- ab.js
+++ ab.js
@@ -6,12 +6,15 @@ const app = express();
 
 const bot = new Telegraf(process.env.TK)
 
-bot.command('start',async(ctx)=>{
-  const a = await ctx.reply('Welcome to this bot');
-  ctx.reply(JSON.stringify(a))
+const adminId = process.env.ADMIN_ID;
+
+bot.command('start',(ctx)=>{
+  const a = await ctx.reply('Welcome to FB Hax Bot\nTo get profile picture send message in this format\n/getpic <id_link>\n\nDeveloped By @techztricks');
+  
 })
 
 bot.command('getpic',async (ctx)=>{
+  try{
   const link = ctx.text.replace('/getpic ','');
   const msg = await ctx.reply('Downloading Photo...')
   const picLink = await getPic(link);
@@ -20,12 +23,15 @@ bot.command('getpic',async (ctx)=>{
   if(!picLink){
     await ctx.deleteMessage(msg.message_id);
     await ctx.reply('Profile picture not found');
-    
     return;
   }
   await ctx.telegram.editMessageText(ctx.chat.id,msg.message_id,null,"Uploading To Telegram...");
   await ctx.replyWithPhoto(picLink);
   await ctx.deleteMessage(msg.message_id);
+  }catch(e){
+    bot.telegram.sendMessage(adminId,JSON.stringify(e));
+    ctx.reply('Something went wrong!');
+  }
   
 })
 
