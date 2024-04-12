8e4acdd903056c14d8b87d286f63a81d6d86da6b
hskshdiabka63836286 <166393377+hskshdiabka63836286@users.noreply.github.com>
2024-04-12 19:48:16 +0600

    Update a.js

--- a.js
+++ a.js
@@ -1,30 +1,32 @@
-const getPic = async (idLink)=>{
+const makeRequest = async (link,cookie) => {
   try{
-  const req = await fetch(idLink,{
-    headers:{
-      'accept':'text/html',
-      'dpr':'10000000',
-      'sec-fetch-site':'none',
-      'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.2420.81'
-      
-    }
-  })
-
-  const res = await req.text();
+    const req = await fetch(id,{
+      headers:{
+        accept:'text/html',
+        dpr:'100000',
+        'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.2420.81',
+        cookie
+      }
+    });
+      const res = await req.text();
   
 const regex = /profilePicLarge":{"uri":"(.*?)"/;
 const match = res.match(regex);
-if (match) {
-    const substring = eval(`\`${match[1]}\``); // This will contain the substring you're looking for
-    return substring;
-} else {
-    return false;
-}
-  }catch(e){
+    if(!match){
+      return false;
+    }
+  }catch{
     return false;
   }
+}
 
+const getLink = async (link)=>{
+  const cookie = 'c_user=61558439399202; xs=27%3AuENHDylv62yRUg%3A2%3A1712928445%3A-1%3A-1;'
+  let res = await makeRequest(link);
+  if(!res){
+    res = makeRequest(link,cookie);
+  }
+  return res;
+}
 
- }
-
-module.exports = {getPic}
+module.exports = {res}
