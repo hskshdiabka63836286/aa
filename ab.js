9eaf1f1e9d965a1b944b274b5cfe8f15bb193e0c
hskshdiabka63836286 <166393377+hskshdiabka63836286@users.noreply.github.com>
2024-04-15 17:43:00 +0600

    Update ab.js

--- ab.js
+++ ab.js
@@ -15,7 +15,7 @@ bot.command('start',(ctx)=>{
   
 })
 
-bot.hears('Get Profile Picture And Cover Photo',()=>{
+bot.hears('Get Profile Picture And Cover Photo',(ctx)=>{
   ctx.session.link = true;
   ctx.reply("Enter Your Profile Link")
 })
